e0796d7485fec062f0177962d266bb9245ba9e78
Zhijian Yao <yao.578@buckeyemail.osu.edu>
2019-05-19 15:37:24 -0400

    Initial Setup

---
+++ SuperMarioBros/MarioGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace SuperMarioBros
+{
+    class MarioGame : Game
+    {
+        private SpriteBatch spriteBatch;
+        public MarioGame()
+        {
+            var graphicsDeviceManager = new GraphicsDeviceManager(this);
+            graphicsDeviceManager.DeviceCreated += (o, e) =>
+            {
+                spriteBatch = new SpriteBatch((o as GraphicsDeviceManager).GraphicsDevice);
+            };
+            Content.RootDirectory = "Content";
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+           
+            base.LoadContent();
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SuperMarioBros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioBros
+{
+    static class Program
+    {
+        static void Main()
+        {
+            using (var game = new MarioGame())
+                game.Run();
+
+        }
+    }
+}
