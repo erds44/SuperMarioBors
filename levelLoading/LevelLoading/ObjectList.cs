a9f4c952fcf3e6651a23473655acd9989256c319
Zhijian Yao <yao.578@buckeyemail.osu.edu>
2019-06-07 17:36:59 -0400

    V3

---
+++ levelLoading/LevelLoading/ObjectList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioBros.LevelLoading
+{
+    public class ObjectList
+    {
+        public  List<ObjectNode> objectList;
+    }
+}
---
+++ levelLoading/LevelLoading/ObjectNode.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarioBros.LevelLoading
+{
+    
+    
+    public  class ObjectNode
+    {
+        public String ObjectType { get; set; }
+        public Point Position { get; set; }
+    }
+}
