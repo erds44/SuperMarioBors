cdde2bb0af6cfd1c308507728ba181f716e03dcc
Keyu Bao <bao.256@buckeyemail.osu.edu>
2019-06-15 12:07:01 -0400

    Levelloading updated

--- SuperMarioBros/ObjectLoading.cs
+++ SuperMarioBros/ObjectLoading.cs
@@ -19,13 +19,35 @@ namespace SuperMarioBros
             objectNodes = content.Load<ObjectList>(path);
             objects = new Collection<IObject>();
             mario = new Collection<IMario> { new Mario(objectNodes.objectList[0].Position) };
-            for (int i = 1; i < objectNodes.objectList.Count; i++)
+            //This part is only for temprorary test
+            Point firstBlock = objectNodes.objectList[1].Position;
+            Type typeofBlock = Type.GetType(objectNodes.objectList[1].ObjectType);
+            Point currentBlock = firstBlock;
+            currentBlock.Y = 440;
+            while (currentBlock.Y < 600)
+            {
+                var obj = Activator.CreateInstance(typeofBlock, currentBlock);
+                objects.Add((IObject)obj);
+                currentBlock.X += 40;
+                if (currentBlock.X >= 800)
+                {
+                    currentBlock.X = 0;
+                    currentBlock.Y += 40;
+
+                }
+            }
+            for (int i = 2; i < objectNodes.objectList.Count; i++)
             {
                 string objectType = objectNodes.objectList[i].ObjectType;
                 Type t = Type.GetType(objectType);
                 Point objectPosition = objectNodes.objectList[i].Position;
                 var obj = Activator.CreateInstance(t, objectPosition);
                 objects.Add((IObject)obj);
+            }
+
+            foreach(IObject obj in objects)
+            {
+                Console.WriteLine(obj.GetType());
             }
         }
         public static Collection<IMario> LoadMario()
